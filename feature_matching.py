31fa39db66838accab785bbf84578a644288f6a7
saifaldin14 <saifaldindeveloper@gmail.com>
2021-01-08 12:04:54 -0500

    Update feature_matching.py

--- feature_matching.py
+++ feature_matching.py
@@ -29,4 +29,60 @@ class FeatureMatching:
         img_query = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         sh_query = img_query.shape
 
-        
+        #key_query = self.f_extractor.detect(img_query)
+        #key_query, desc_query = self.f_extractor.compute(img_query, key_query)
+        key_query, desc_query = self.f_extractor.detectAndCompute(img_query, None)
+        good_matches = self.match_features(desc_query)
+        train_points = [self.key_train[good_match.queryIdx].pt
+                        for good_match in good_matches]
+        query_points = [key_query[good_match.trainIdx].pt
+                        for good_match in good_matches]
+
+    def match_features(self, desc_frame: np.ndarray) -> List[cv.DMatch]:
+        matches = self.flann.knnMatch(self.desc_train, desc_frame, k=2)
+        good_matches = [x[0] for x in matches
+                        if x[0].distance < 0.7 * x[1].distance]
+        return good_matches
+
+def detect_corner_points(src_points: Sequence[Point],
+                         dst_points: Sequence[Point],
+                         sh_src: Tuple[int, int]) -> np.ndarray:
+    H, _ = cv.findHomography(np.array(src_points), np.array(dst_points),
+                                      cv.RANSAC)
+    if H is None:
+        raise Outlier("Homography not fount")
+    height, width = sh_src
+    src_corners = np.array([(0,0), (width, 0),
+                            (width, height),
+                            (0, height)], dtype=np.float32)
+    return cv.perspectiveTransform(src_corners[None, :, :], H)[0]
+
+def draw_good_matches(self, img1: np.ndarray,
+                      kp1: Sequence(cv.KeyPoint),
+                      img2: np.ndarray,
+                      kp2: Sequence(cv.KeyPoint),
+                      matches: Sequence[cv.DMatch]) -> np.ndarray:
+    rows1, cols1 = img1.shape[:2]
+    rows2, cols2 = img2.shape[:2]
+    out = np.zeros((max([rows1, rows2]), cols1 + cols2, 3), dtype='uint8')
+
+    out[:rows1, :cols1, :] = img1[..., None]
+    out[:rows2, cols1: cols1 + cols2, :] = img2[..., None]
+
+    for m in matches:
+        c1 = tuple(map(int, kp1[m.queryIdx].pt))
+        c2 = tuple(map(int, kp2[m.trainIdx].pt))
+        c2 = c2[0] + cols1, c2[1]
+
+        radius = 4
+        BLUE = (255, 0, 0)
+        thickness = 1
+
+        cv.circle(out, c1, radius, BLUE, thickness)
+        cv.circle(out, c2, radius, BLUE, thickness)
+
+        cv.line(out, c1, c2, BLUE, thickness)
+    return out
+
+
+
